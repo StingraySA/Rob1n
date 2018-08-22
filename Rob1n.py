a68c25f8433117f61e636f22ef8f27cf40ef7d09
StingraySA <42263294+StingraySA@users.noreply.github.com>
2018-08-22 11:33:35 +0200

    Add files via upload

--- Rob1n.py
+++ Rob1n.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python2
-from external_scan import external_scan
-from external_scan import scanport
-from external_scan import openports
+import lib
+from lib.external_scan import external_scan
+from lib.external_scan import scanport
+from lib.external_scan import openports
+from lib.external_scan import bcolors
 import sys
 
 target = str(sys.argv[1:])
@@ -10,8 +12,20 @@ target = target.replace("]","")
 target = target.replace(",","")
 target = target.replace("'","")
 
+print(bcolors.OKGREEN + '__________      ___.   ____        ')
+print(bcolors.OKGREEN + '\______   \ ____\_ |__/_   | ____  ')
+print(bcolors.OKGREEN + ' |       _//  _ \| __ \|   |/    \ ')
+print(bcolors.OKGREEN + ' |    |   (  (_) ) \_\ \   |   |  \\')
+print(bcolors.OKGREEN + ' |____|_  /\____/|___  /___|___|  /')
+print(bcolors.OKGREEN + '        \/           \/         \/ ')
+print(bcolors.OKGREEN + '      The Pentesters Sidekick      ')
+print(bcolors.OKGREEN + '          By: StingraySA           ')
+print(bcolors.OKGREEN + '-----------------------------------' + bcolors.ENDC)
+
+# Initialise everything for the external scan
 external_scan(target)
 
+# Perform the external scan on all open ports
 x = 0
 if len(openports) != '':
 	while x < len(openports):
